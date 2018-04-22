3547987d85e5adf4714ff2c2911c51a94663b870
EnesOnurAta <38593480+EnesOnurAta@users.noreply.github.com>
2018-04-22 10:23:20 +0300

    Giriş çıkış eklendi
    #Terbiyesiz kanalınız varsa bot oraya mesaj atacaktır.

--- bot.js
+++ bot.js
@@ -203,3 +203,32 @@ bot.on('message', msg => {
       msg.channel.send(embed)
     }
   });*/
+
+/* Giriş Çıkış */
+client.on('guildMemberAdd', member => {
+  let guild = member.guild;
+  let joinRole = guild.roles.find('name', 'Sapık'); // Burada girişte verilcek rolu seçelim.
+  member.addRole(joinRole); // seçtiğimiz rolu verelim.
+
+  const channel = member.guild.channels.find('name', 'Terbiyesiz'); // burda ise kanalı belirleyelim hangi kanala atsın ben mod-log dedim.
+  if (!channel) return;
+  const embed = new Discord.RichEmbed()
+  .setColor('RANDOM')
+  .setAuthor(member.user.username, member.user.avatarURL)
+  .setThumbnail(member.user.avatarURL)
+  .setTitle('📥 | Sunucuya katıldı!')
+  .setTimestamp()
+  channel.sendEmbed(embed); // belirlediğimiz kanala mesaj gönderelim.
+});
+
+client.on('guildMemberRemove', member => {
+  const channel = member.guild.channels.find('name', 'Terbiyesiz');
+  if (!channel) return;
+  const embed = new Discord.RichEmbed()
+  .setColor('RANDOM')
+  .setAuthor(member.user.username, member.user.avatarURL)
+  .setThumbnail(member.user.avatarURL)
+  .setTitle('📤 | Sunucudan ayrıldı | Görüşmek üzere!')
+  .setTimestamp()
+  channel.sendEmbed(embed); 
+});
